18020e30f9b92da67e4ed88e1cc0830dc4e9b063
Pooja <m.pooja1863@gmail.com>
2025-04-12 14:53:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,102 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import {Component} from 'react'
+import TodoItem from '../TodoItem'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+class SimpleTodos extends Component {
+  state = {
+    todosList: [
+      {id: 1, title: 'Task 1', completed: false},
+      {
+        id: 2,
+        title: 'Task 2',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Task 3',
+        completed: false,
+      },
+      {id: 4, title: 'Task 4', completed: false},
+      {id: 5, title: 'Task 5', completed: false},
+      {id: 6, title: 'Task 6', completed: false},
+      {id: 7, title: 'Task 7', completed: false},
+      {id: 8, title: 'Task 8', completed: false},
+    ],
+    newTodoTitle: '',
+    newTodoCount: 1,
+  }
+
+  handleAddTodo = () => {
+    const {newTodoTitle, newTodoCount} = this.state
+    const newTodos = Array.from({length: newTodoCount}, (_, i) => ({
+      id: Date.now() + i,
+      title: newTodoTitle,
+      completed: false,
+    }))
+    this.setState(prevState => ({
+      todosList: [...prevState.todosList, ...newTodos],
+      newTodoTitle: '',
+      newTodoCount: 1,
+    }))
+  }
+
+  handleChange = e => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  deleteTodo = id => {
+    const {todosList} = this.state
+    const updatedTodoList = todosList.filter(todo => todo.id !== id)
+    this.setState({todosList: updatedTodoList})
+  }
+
+  toggleComplete = id => {
+    const {todosList} = this.state
+    const updatedTodoList = todosList.map(todo =>
+      todo.id === id ? {...todo, completed: !todo.completed} : todo,
+    )
+    this.setState({todosList: updatedTodoList})
+  }
+
+  render() {
+    const {todosList, newTodoTitle, newTodoCount} = this.state
+    return (
+      <div className="container">
+        <div className="inner-container">
+          <h1 className="heading">Simple Todos</h1>
+          <div className="add-todo">
+            <input
+              type="text"
+              name="newTodoTitle"
+              value={newTodoTitle}
+              onChange={this.handleChange}
+              placeholder="Enter todo title"
+            />
+            <input
+              type="number"
+              name="newTodoCount"
+              value={newTodoCount}
+              onChange={this.handleChange}
+              placeholder="Enter number of todos"
+            />
+            <button onClick={this.handleAddTodo} type="button">
+              Add
+            </button>
+          </div>
+          <ul className="todos-list">
+            {todosList.map(todo => (
+              <TodoItem
+                key={todo.id}
+                todoDetails={todo}
+                deleteTodo={this.deleteTodo}
+                toggleComplete={this.toggleComplete}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SimpleTodos
